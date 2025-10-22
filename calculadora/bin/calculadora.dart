31625f2b1902c0af8ebef65d25865650b5adca9f
Enzo-Maranzatti <enzomaranzattisilva@gmail.com>
2025-10-22 16:25:35 -0300

    Atualizando calculadora, usando condicionais

--- calculadora/bin/calculadora.dart
+++ calculadora/bin/calculadora.dart
@@ -4,6 +4,8 @@ void main() {
   print("----------------------CALCULADORA----------------------\n");
   double numeroUm = double.parse(stdin.readLineSync()!);
   double numeroDois = double.parse(stdin.readLineSync()!);
+  String operacao = stdin.readLineSync()!;
+
 
   void soma() { 
     print(numeroUm + numeroDois);
@@ -21,8 +23,36 @@ void main() {
     print(numeroUm * numeroDois);
   }
 
-  soma();
-  subtracao();
-  divisao();
-  multiplicacao();
+  // Maneira 3 - Switch
+  switch(operacao) {
+    case "+": soma(); break;
+    case "-": subtracao(); break;
+    case "/": divisao(); break;
+    case "*": multiplicacao(); break;
+    default: print("Entrada inválida!!");
+
+    /*
+      // Maneira 1 - IF
+
+      if(operacao == "+") { soma(); }
+      if(operacao == "-") { subtracao(); }
+      if(operacao == "/") { divisao(); }
+      if(operacao == "*") { multiplicacao(); }
+
+      ----------------------------------------
+
+      // Maneira 2 - IF/ELSE IF
+
+      if(operacao == "+") { 
+        soma();
+      } else if(operacao == "-") {
+        subtracao();
+      } else if(operacao == "/"){
+        divisao();
+      } else if(operacao == "*") {
+        multiplicacao();
+      }    
+
+    */
+  }
 }
