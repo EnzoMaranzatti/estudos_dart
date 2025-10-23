bf526623ba180dc311212e6e32dc085cd5dbad38
Enzo-Maranzatti <enzomaranzattisilva@gmail.com>
2025-10-23 16:11:19 -0300

    Finalizando calculadora

--- calculadora/bin/calculadora.dart
+++ calculadora/bin/calculadora.dart
@@ -5,75 +5,49 @@ void main() {
   double numeroUm = 0;
   double numeroDois = 0;
   String operacao = "";
-  // List<Tipo> nomeLista = <tipo>[...];
+  String? entrada = "";
   List<String> operacoes = <String>["+", "-", "/", "*"];
 
+  void soma() => print(numeroUm + numeroDois);
+  void subtracao() => print(numeroUm - numeroDois);
+  void divisao() => print(numeroUm / numeroDois);
+  void multiplicacao() => print(numeroUm * numeroDois);
 
-  void soma() { 
-    print(numeroUm + numeroDois);
-  }
-
-  void subtracao() {
-    print(numeroUm - numeroDois);
-  }
-
-  void divisao() {
-    print(numeroUm / numeroDois);
-  }
-
-  void multiplicacao() {
-    print(numeroUm * numeroDois);
+  void getOperacao() {
+    print("Digite uma operação ${operacoes.toString()}");
+    entrada = stdin.readLineSync();
+    if (entrada != null && operacoes.contains(entrada)) {
+      operacao = entrada!;
+    } else {
+      print("entrada inválida");
+      getOperacao();
+    }
   }
 
   void calcular() {
-    // Maneira 4 - Switch
-    switch(operacao) {
+    switch (operacao) {
       case "+": soma(); break;
       case "-": subtracao(); break;
       case "/": divisao(); break;
       case "*": multiplicacao(); break;
-      default: print("Entrada inválida!!"); 
+      default: print("Entrada inválida!");
     }
   }
 
-  print("Digite o primeiro valor");
-
-  String? entrada = stdin.readLineSync();
-
-  if(entrada != null) {
-    if(entrada != "") {
-      numeroUm = double.parse(entrada);
-    }
+  print("Digite o primeiro valor:");
+  entrada = stdin.readLineSync(); 
+  if (entrada != null && entrada != "") {
+    numeroUm = double.parse(entrada!);
   }
 
-  void getOperacao() {
-    print("Digite uma operação");
-    entrada = stdin.readLineSync();
+  getOperacao();
 
-    if(entrada != null) {
-      if(operacoes.contains(entrada)) {
-        operacao = entrada!;
-      }
-    }
-  }
-
-  print("Digite uma operação");
-
+  print("Digite o segundo valor:");
   entrada = stdin.readLineSync();
-  if(entrada != null) {
-    operacao = entrada!;
-  }
-
-  print("Digite o segundo valor");
-
-  entrada = stdin.readLineSync();
-  if(entrada != null) {
-    if(entrada != "") {
-      numeroDois = double.parse(entrada!);
-    }
+  if (entrada != null && entrada != "") {
+    numeroDois = double.parse(entrada!);
   }
 
   print("O resultado da operação é:");
-
   calcular();
 }
