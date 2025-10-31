c63b12d7cbeab6a0676aa8fcbc1cb9566d6ba7e8
Enzo-Maranzatti <enzomaranzattisilva@gmail.com>
2025-10-31 07:56:45 -0300

    Adicionando exercicios

---
+++ orientacao_a_objeto/exercicios/agendamento/README.md
@@ -0,0 +1,5 @@
+# Definindo a interface para agendamento
+
+Crie uma interface chamada Agendamento que inclua os métodos calculaDuracaoConsulta() e verificaDisponibilidade(). Esses métodos devem ser abstratos e servem como base para diferentes tipos de profissionais de saúde implementarem suas regras de agendamento.
+
+Você não precisa exibir nada no terminal! Apenas crie o código com a estrutura da interface.
---
+++ orientacao_a_objeto/exercicios/agendamento/lib/agendamento.dart
@@ -0,0 +1,43 @@
+abstract class Agendamento {
+  calculaDuracaoConsulta();
+  verificaDisponibilidade();
+}
+
+class Medico implements Agendamento {
+  @override
+  calculaDuracaoConsulta() {
+    print("A duração de uma consulta médica é de 30 minutos");
+  }
+
+  @override
+  verificaDisponibilidade() {
+    print("A disponibilidade é verificada com base em um calendário de agendamentos previamente existente.");
+  }
+}
+
+class Dentista implements Agendamento {
+  @override
+  calculaDuracaoConsulta() {
+    print("A duração de uma consulta médica é de 45 minutos")
+  }
+
+  @override
+  verificaDisponibilidade() {
+    print("Dentistas têm intervalos de 15 minutos entre consultas.")
+  }
+}
+
+class GerenciadorDeAgendamentos {
+  List<Agendamento> profissionais = [];
+
+  void adicionarProfissionais(Agendamento profissional) {
+    profissionais.add(profissional);
+  }
+
+  void exibirDuracaoEDisponibilidade() {
+    for (Agendamento profissional in profissionais) {
+      profissional.calculaDuracaoConsulta();
+      profissional.verificaDisponibilidade();
+    }
+  }
+}
