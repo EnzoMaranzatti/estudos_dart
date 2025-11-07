1788d240602d20e2515c14d847053c05dc7a21b5
Vinicius-Maranzatti <enzomaranzattisilva@gmail.com>
2025-11-07 11:56:43 -0300

    Atualizando projeto async

--- async/dart_assincronismo/bin/main.dart
+++ async/dart_assincronismo/bin/main.dart
@@ -1,13 +1,27 @@
 import 'package:http/http.dart';
+import 'dart:convert';
 
 void main() {
-  requestData();
+  requestDataAsync();
 }
 
 requestData() {
   String url = "https://gist.githubusercontent.com/EnzoMaranzatti/6070ea5a79c14ad2b087910129a23a35/raw/7a4930a5b29d40516b782ae31283041f0cae5461/accounts.json";
   Future<Response> futureResponse = get(Uri.parse(url));
   futureResponse.then((Response response) {
+    print(response);
     print(response.body);
+    List<dynamic> listaAccount = json.decode(response.body);
+    Map<String, dynamic> mapCarla = listaAccount.firstWhere((element) => element["name"] == "Carla");
+    print(mapCarla["balance"]);
   },);
+
+  print("Ultima coisa a acontecer na função");
+}
+
+requestDataAsync() async {
+  print("Teste");
+  String url = "https://gist.githubusercontent.com/EnzoMaranzatti/6070ea5a79c14ad2b087910129a23a35/raw/7a4930a5b29d40516b782ae31283041f0cae5461/accounts.json";
+  Response response = await get(Uri.parse(url));
+  print(json.decode(response.body));
 }
