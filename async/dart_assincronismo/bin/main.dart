7326b777fa5447e70c9e5811a3f823363bbb2cf0
Enzo Maranzatti <enzoviniciusmaranzattidasilva@MacBook-Air-de-Enzo.local>
2025-11-11 20:11:26 -0300

    Atualizando projeto accounts e adicionando exercicio

--- async/dart_assincronismo/bin/main.dart
+++ async/dart_assincronismo/bin/main.dart
@@ -2,7 +2,12 @@ import 'package:http/http.dart';
 import 'dart:convert';
 
 void main() {
-  requestDataAsync();
+  sendDataAsync({
+    "id": "001",
+    "name": "Flutter",
+    "lastName": "Dart",
+    "balance": 5000,
+  });
 }
 
 requestData() {
@@ -18,10 +23,19 @@ requestData() {
 
   print("Ultima coisa a acontecer na função");
 }
-
-requestDataAsync() async {
+// Todo retorno de uma função async tem que ser um Futere
+Future<List<dynamic>> requestDataAsync() async {
   print("Teste");
   String url = "https://gist.githubusercontent.com/EnzoMaranzatti/6070ea5a79c14ad2b087910129a23a35/raw/7a4930a5b29d40516b782ae31283041f0cae5461/accounts.json";
   Response response = await get(Uri.parse(url));
-  print(json.decode(response.body));
+  return json.decode(response.body);
+}
+
+sendDataAsync(Map<String, dynamic> mapAccount) async {
+  List<dynamic> listaAccounts = await requestDataAsync();
+  listaAccounts.add(mapAccount);
+  String content = json.encode(listaAccounts);
+  
+  String url = "https://gist.githubusercontent.com/EnzoMaranzatti/6070ea5a79c14ad2b087910129a23a35/raw/7a4930a5b29d40516b782ae31283041f0cae5461/accounts.json";
+  Response response = await post(Uri.parse(url), body: content); 
 }
---
+++ async/exercicios/dart_receitas/bin/dart_receitas.dart
@@ -0,0 +1,5 @@
+import 'package:dart_receitas/dart_receitas.dart' as dart_receitas;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_receitas.calculate()}!');
+}
